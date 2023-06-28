8bed3ebc93b7935da48792560bb71d13eee46392
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-06-28 10:47:37 +0500

    solution problem #125

---
+++ 125. Valid Palindrome.go
@@ -0,0 +1,39 @@
+package leetcode
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(isPalindrome("race a car"))
+	fmt.Println(isPalindrome(" "))
+	fmt.Println(isPalindrome("!!!"))
+}
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	i, j := 0, len(s)-1
+	for i < j {
+		for i < j && !isChar(s[i]) {
+			i++
+		}
+		for i < j && !isChar(s[j]) {
+			j--
+		}
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+
+	return true
+}
+
+func isChar(c byte) bool {
+	if ('a' <= c && c <= 'z') || ('0' <= c && c <= '9') {
+		return true
+	}
+	return false
+}
